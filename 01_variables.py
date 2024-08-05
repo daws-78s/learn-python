1f26c8c05732132170996990337aed77812801b7
Pavan Kumar Akula <akulapavankumar@outlook.com>
2024-08-05 16:48:19 +0200

    Some more examples

--- 01_variables.py
+++ 01_variables.py
@@ -5,7 +5,30 @@ this is a multi-line comment
 """
 
 # Define a variable
-a = 42
+a = 42 # This is a integer variable
 
 # Print the value that is stored inside the variable
 print(a)
+
+"""
+- Boolean
+- Strings
+"""
+
+# Float value
+b = 42.345
+print(b)
+
+# Boolean
+c = True # true
+
+# Strings
+d = 'This is a string'
+e = "This is also a string"
+f = """
+This is a multi line string
+""""
+
+# Today's weather is nice
+d = 'Today's weather is nice'
+print(d)
