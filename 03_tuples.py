eef0059437977d257d0f9b3e0e3ef04856f3c260
Pavan Kumar Akula <akulapavankumar@outlook.com>
2024-08-06 17:05:06 +0200

    Sample tuple

--- 03_tuples.py
+++ 03_tuples.py
@@ -2,4 +2,14 @@
 # 1. ()
 # 2. tuple()
 # Behavior: They're immutable
-sample_tuple = ("") 
+sample_tuple = ("Ansible", "Terraform", "Jenkins", "Docker", "K8s") # Tool set
+
+sample_ele = sample_tuple[1]
+print(sample_ele)
+
+sample_ele = sample_tuple[-1]
+print(sample_ele)
+
+# Slicing
+sliced_tuple = sample_tuple[1:3] # ("Terraform", "Jenkins")
+print(sliced_tuple)
