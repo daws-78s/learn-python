8f89f79c1e5d640b8e08124e1072af135b7946f5
Pavan Kumar Akula <akulapavankumar@outlook.com>
2024-08-06 16:37:48 +0200

    String length

--- 02_strings.py
+++ 02_strings.py
@@ -20,5 +20,10 @@ print(sub_str)
 sub_str = sample_str[::2]
 print(sub_str)
 
+# Reverse a string
 sub_str = sample_str[::-1]
-print(sub_str)
+print("Reversed string:", sub_str)
+
+# Length of a string
+len_str = len(sample_str)
+print("Length of a string:", len_str)
