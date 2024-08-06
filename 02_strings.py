9aef07c6879db71f5ee9d2e39636d0b76cb7f88f
Pavan Kumar Akula <akulapavankumar@outlook.com>
2024-08-06 16:49:23 +0200

    Split

--- 02_strings.py
+++ 02_strings.py
@@ -31,3 +31,8 @@ print("Length of a string:", len_str)
 # Method
 sample_str = "hello"
 print(sample_str.capitalize()) # "Hello"
+
+# split(), join(), format(), count(), strip()
+sample_str = "This is a sample string"
+str_split = sample_str.split() # output: list
+print(sample_str, type(str_split))
