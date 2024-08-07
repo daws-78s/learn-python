26a3142d67d941715e1907a90cb95579af423640
Pavan Kumar Akula <akulapavankumar@outlook.com>
2024-08-07 16:50:55 +0200

    Access elements

--- 06_sets.py
+++ 06_sets.py
@@ -3,8 +3,12 @@ print(sample_set)
 
 # A Set returns unique elements that is stored inside that variable
 # Sets don't consider the order of insertion
+# Sets don't support indexing
 
 # Add elements to a set
 sample_set = {1, 2, 5, 2, 4, 3}
 sample_set.add(6)
 print(sample_set)
+print(sample_set[2])
+
+# intersection(), union()
