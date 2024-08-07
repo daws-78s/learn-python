11ed908a9e25f003aaff934867ad18b215e49841
Pavan Kumar Akula <akulapavankumar@outlook.com>
2024-08-07 16:48:14 +0200

    Add elements to set

--- 06_sets.py
+++ 06_sets.py
@@ -1,2 +1,10 @@
 sample_set = {1, 2, 5, 2, 4, 3} # set()
 print(sample_set)
+
+# A Set returns unique elements that is stored inside that variable
+# Sets don't consider the order of insertion
+
+# Add elements to a set
+sample_set = {1, 2, 5, 2, 4, 3}
+sample_set.add(6)
+print(sample_set)
