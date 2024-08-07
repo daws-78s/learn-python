dd93a2b41ca033a3a06978f1a3d9a7a254b76a76
Pavan Kumar Akula <akulapavankumar@outlook.com>
2024-08-07 16:55:51 +0200

    Type casting

--- 06_sets.py
+++ 06_sets.py
@@ -9,6 +9,13 @@ print(sample_set)
 sample_set = {1, 2, 5, 2, 4, 3}
 sample_set.add(6)
 print(sample_set)
-print(sample_set[2])
+# print(sample_set[2])
+
+""" 
+Traceback (most recent call last):
+  File "/home/cloudshell-user/python-devops/06_sets.py", line 12, in <module>
+    print(sample_set[2])
+TypeError: 'set' object is not subscriptable 
+"""
 
 # intersection(), union()
---
+++ 07_type_conversion.py
@@ -0,0 +1,10 @@
+sample_str = "This is a sample string"
+sample_str_list = list(sample_str)
+print(sample_str_list)
+
+sample_str_tuple = tuple(sample_str)
+print(sample_str_tuple)
+
+# Accept input from a user
+user_input = input("Enter a number:")
+print(user_input, type(user_input))
