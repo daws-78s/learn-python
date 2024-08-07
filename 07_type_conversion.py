f58dd382ebcd3cde9a411600548f456938aef4ed
Pavan Kumar Akula <akulapavankumar@outlook.com>
2024-08-07 17:01:08 +0200

    Comment error

--- 07_type_conversion.py
+++ 07_type_conversion.py
@@ -9,8 +9,14 @@ print(sample_str_tuple)
 user_input = input("Enter a number:")
 print(user_input, type(user_input))
 
-add_10 = int(user_input) + 10
-print(add_10)
+# add_10 = int(user_input) + 10
+# print(add_10)
+
+"""
+Traceback (most recent call last):
+  File "/home/cloudshell-user/python-devops/07_type_conversion.py", line 12, in <module>    add_10 = int(user_input) + 10
+ValueError: invalid literal for int() with base 10: '10 100 200 500 700'
+"""
 
 split_input = "10 100 200 500 700".split(" ")
 print(split_input)
