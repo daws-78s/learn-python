2a800a3b83ae841d231dcc3c245659e095abc4af
Pavan Kumar Akula <akulapavankumar@outlook.com>
2024-08-08 16:46:17 +0200

    Range based

--- 10_loops.py
+++ 10_loops.py
@@ -10,8 +10,9 @@ sample_list = ["Ansible", "Terraform", "Jenkins", "Docker", "K8s"]
 
 # Range based for loop
 # 1:10 -> 1,2...,9
-sample_range = range(0, len(sample_list))
-print(sample_range, type(sample_range))
 
-for idx in sample_range:
-    print(idx)
+# sample_range = range(0, len(sample_list))
+# print(sample_range, type(sample_range))
+
+for idx in range(0, len(sample_list)):
+    print(idx, sample_list[idx])
