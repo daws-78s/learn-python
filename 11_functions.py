bd65ca202eb66182b9791dded9b3775cd7580372
Pavan Kumar Akula <akulapavankumar@outlook.com>
2024-08-08 17:05:26 +0200

    Docstring

--- 11_functions.py
+++ 11_functions.py
@@ -5,9 +5,13 @@
 # sample_func()
 
 def add(num_1, num_2):
+    """
+    This function performs addition of 2 numbers
+    """
     res = num_1 + num_2
     return res
 
 # Call the function
-res = add(1, 2)
+# res = add(1, 2)
+res = add(num_2=1, num_1=2)
 print(res)
