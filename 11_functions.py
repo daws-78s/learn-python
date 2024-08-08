f6e7ea23dcd744a6267613ca35770972e68c69fc
Pavan Kumar Akula <akulapavankumar@outlook.com>
2024-08-08 17:08:04 +0200

    default params

--- 11_functions.py
+++ 11_functions.py
@@ -4,16 +4,28 @@
 # # Call the function
 # sample_func()
 
-def add(num_1, num_2):
+# def add(num_1, num_2):
+#     """
+#     This function performs addition of 2 numbers
+#     """
+#     res = num_1 + num_2
+#     return res
+
+# # Call the function
+# # res = add(1, 2)
+# res = add(num_2=1, num_1=2)
+# print(res)
+
+# help(add)
+
+def add(num_1, num_2, num_3=10):
     """
     This function performs addition of 2 numbers
     """
-    res = num_1 + num_2
+    res = num_1 + num_2 + num_3
     return res
 
 # Call the function
 # res = add(1, 2)
 res = add(num_2=1, num_1=2)
 print(res)
-
-help(add)
