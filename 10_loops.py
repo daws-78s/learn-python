9c6a7f5dec83425057ecb902b1230cbe4d94b7fb
Pavan Kumar Akula <akulapavankumar@outlook.com>
2024-08-08 16:41:05 +0200

    Enumerate

--- 10_loops.py
+++ 10_loops.py
@@ -5,5 +5,5 @@ sample_list = ["Ansible", "Terraform", "Jenkins", "Docker", "K8s"]
 
 # Print the element and its corresponding index
 
-for ele in enumerate(sample_list):
-    print(ele)
+for idx, ele in enumerate(sample_list):
+    print(idx, ele)
