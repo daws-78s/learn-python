6310cddb3ab9bbe77f8add229236b52a278f0929
Pavan Kumar Akula <akulapavankumar@outlook.com>
2024-08-08 16:22:17 +0200

    Conditionals

--- 08_conditionals.py
+++ 08_conditionals.py
@@ -1,15 +1,41 @@
-# Calculate whether the given number is greater than 10 or not
+# Check whether the given number is greater than 10 or not
 
 user_input = int(input("Enter a number:"))
 # user_input = int(user_input)
 
-if user_input > 10:
+# if user_input > 10:
+#     print("User input value is greater than 10")
+# else:
+#     print("User input value is less than or equal 10")
+
+# Check whether the given number is equal to 10 or greater than 10 or less than 10 or something else
+
+# Method 1
+if user_input == 10:
+    print("Right on target")
+else:
+    if user_input > 10:
+        print("User input value is greater than 10")
+    else:
+        if user_input < 10:
+            print("User input value is less than 10")
+
+# Method 2
+if user_input == 10:
+    print("Right on target")
+elif user_input > 10:
     print("User input value is greater than 10")
 else:
-    print("User input value is less than or equal 10")
+    print("User input value is less than 10")
 
-"""
-{
-
-}
-"""
+# Exception handling: try - except
+try:
+    user_input = int(input("Enter a number:"))
+    if user_input == 10:
+        print("Right on target")
+    elif user_input > 10:
+        print("User input value is greater than 10")
+    else:
+        print("User input value is less than 10")
+except:
+    print("Please enter a number")
