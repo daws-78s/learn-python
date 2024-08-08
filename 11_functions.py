eeeadb4554cbcaa340a84b3fcf78db9ecda4ae06
Pavan Kumar Akula <akulapavankumar@outlook.com>
2024-08-08 17:03:08 +0200

    Pass arguments

--- 11_functions.py
+++ 11_functions.py
@@ -1,5 +1,13 @@
-def sample_func():
-    print("This is a sample function")
+# def sample_func():
+#     print("This is a sample function")
+
+# # Call the function
+# sample_func()
+
+def add(num_1, num_2):
+    res = num_1 + num_2
+    return res
 
 # Call the function
-sample_func()
+res = add(1, 2)
+print(res)
