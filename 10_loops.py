47408d4089146a02efbbea6b5f5fa828a5e1855f
Pavan Kumar Akula <akulapavankumar@outlook.com>
2024-08-08 16:42:54 +0200

    Range

--- 10_loops.py
+++ 10_loops.py
@@ -5,5 +5,10 @@ sample_list = ["Ansible", "Terraform", "Jenkins", "Docker", "K8s"]
 
 # Print the element and its corresponding index
 
-for idx, ele in enumerate(sample_list):
-    print(idx, ele)
+# for idx, ele in enumerate(sample_list):
+#     print(idx, ele)
+
+# Range based for loop
+# 1:10 -> 1,2...,9
+sample_range = range(0, len(sample_list))
+print(sample_range)
