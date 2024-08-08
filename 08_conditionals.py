28a31904dea05421a3d15922a301ab188a619bd7
Pavan Kumar Akula <akulapavankumar@outlook.com>
2024-08-08 16:10:47 +0200

    Conditions example

---
+++ 08_conditionals.py
@@ -0,0 +1,15 @@
+# Calculate whether the given number is greater than 10 or not
+
+user_input = int(input("Enter a number:"))
+# user_input = int(user_input)
+
+if user_input > 10:
+    print("User input value is greater than 10")
+else:
+    print("User input value is less than or equal 10")
+
+"""
+{
+
+}
+"""
